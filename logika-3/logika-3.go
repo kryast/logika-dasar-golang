accb4d00f88881225d46abac36f3274c5f1bd2e7
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-09-22 17:50:13 +0700

    Logika 3

--- logika-3/logika-3.go
+++ logika-3/logika-3.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// Soal 1: pisahkan angka Ganjil dan Genap dari angka 1 sd 100
+
+	var Genap []int  // buat array untuk menampung angka genap
+	var Ganjil []int // buat array untuk menampung angka ganjil
+
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			Genap = append(Genap, i) // append(array, nilai yg akan di masukkan ke array)
+		} else {
+			Ganjil = append(Ganjil, i)
+		}
+	}
+
+	fmt.Println("angka genap :", Genap)
+	fmt.Println("angka ganjil :", Ganjil)
 }
