a4e6e0264b85bced3bad45f65c8c646ccf287d3e
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-09-23 17:42:16 +0700

    Logika 12

---
+++ logika-12/logika-12.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	/**
+	soal :
+	buatlah output
+	**********
+	*       **
+	*      * *
+	*     *  *
+	*    *   *
+	*   *    *
+	*  *     *
+	* *      *
+	**       *
+	**********
+	dari input angka 10
+	*/
+
+	// Mendefinisikan variabel 'kotak' dengan nilai 10
+	kotak := 10
+
+	// Loop pertama untuk mengiterasi setiap baris dari 0 hingga 'kotak'
+	for i := 0; i <= kotak; i++ {
+		// Loop kedua untuk mengiterasi setiap kolom dari 0 hingga 'kotak - 1'
+		for j := 0; j < kotak; j++ {
+			// Memeriksa apakah kita berada di baris pertama, baris terakhir, kolom pertama, atau di diagonal yang menurun
+			if i == 0 || i == kotak || j == 0 || j == kotak-i {
+				// Jika syarat terpenuhi, cetak bintang (*)
+				fmt.Print("*")
+			} else {
+				// Jika tidak, cetak spasi untuk membentuk area kosong di dalam pola
+				fmt.Print(" ")
+			}
+		}
+		// Mencetak bintang (*) di akhir baris untuk menutup sisi kanan dari pola
+		fmt.Println("*")
+	}
+}
